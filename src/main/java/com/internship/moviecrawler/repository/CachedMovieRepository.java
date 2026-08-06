caac62efb8e1867fd687c5f7ddf7cc610fb2900f
Quang Dzung Pham <132767821+quanggpham@users.noreply.github.com>
2026-08-06 07:00:13 +0700

    Add comment about changes on GitHub server

--- src/main/java/com/internship/moviecrawler/repository/CachedMovieRepository.java
+++ src/main/java/com/internship/moviecrawler/repository/CachedMovieRepository.java
@@ -65,4 +65,5 @@ public class CachedMovieRepository implements MovieRepository {
 
     /** Expose current cache size (including expired entries not yet evicted). */
     public int getCacheSize()                         { return (int) cache.size(); }
+    /* Chinh sua tren server github */
 }
